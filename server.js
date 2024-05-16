d1c4707a4d9c950ba9dfff5d7d66e19b6d5652c0
NarenSiddharth <narensiddharth@gmail.com>
2024-05-16 17:07:24 +0530

    dont know

--- server.js
+++ server.js
@@ -5,15 +5,29 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
     host : "localhost",
     user : "root",
-    password : "Naren362004@"
+    password : "Naren362004@",
+    database : "IplTicket" 
+});
 
-})
+
 
 
 const app = express();
 
+app.get('/', function(req,res){
+    let sql = "Select * from matches";
+    db.query(sql,function(err,results){
+        if(err) throw err
+        res.send(results);
+    });
+})
+
 app.listen(8081, () => {
-    console.log("listening")
+    console.log("listening");
+    db.connect(function(err){
+        if(err) throw err;
+        console.log("Database connected");
+    })
 
 })
 
